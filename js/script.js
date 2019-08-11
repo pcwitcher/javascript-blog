d2b4df22f359932a00638b08442b583c503f6659
pcwitcher <pcwitcher.ie@gmail.com>
2019-08-11 22:59:17 +0200

    remove class active from articles

--- js/script.js
+++ js/script.js
@@ -10,9 +10,21 @@ const titleClickHandler = function () {
 
     /* remove class 'active' from all article links  */
 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('href');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
